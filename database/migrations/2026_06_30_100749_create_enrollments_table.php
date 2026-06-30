0baa2a374c8b865d58956ad010ad855cba5ce416
Fatma Alaa <fatalaa1@gmail.com>
2026-06-30 13:46:37 +0300

    add enrollments table

---
+++ database/migrations/2026_06_30_100749_create_enrollments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignUuid('course_id')->constrained('courses')->onDelete('cascade');
+            $table->string('status')->default('active');
+            $table->timestamp('enrolled_at')->useCurrent();
+            $table->timestamp('completed_at')->nullable();
+            $table->unsignedTinyInteger('progress')->default(0);
+            $table->timestamps();
+            $table->unique(['user_id', 'course_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
